69327aa7bd7a588d9dbc79faf4833c96f9bfc049
barbok211 <barbarianz@hotmail.fr>
2016-03-03 15:05:44 +0100

    Partial commit

---
+++ src/java/fr/epsi/mtp/i4/c22016/ThemeSwitcher/PickListView.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.epsi.mtp.i4.c22016.ThemeSwitcher;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import org.primefaces.event.SelectEvent;
+ 
+import org.primefaces.event.TransferEvent;
+import org.primefaces.event.UnselectEvent;
+import org.primefaces.model.DualListModel;
+
+@Named(value = "pickListView")
+@ManagedBean
+@SessionScoped
+public class PickListView {
+          
+    private DualListModel<String> cities;
+     
+    @PostConstruct
+    public void init() {
+        //Cities
+        List<String> citiesSource = new ArrayList<String>();
+        List<String> citiesTarget = new ArrayList<String>();
+         
+        citiesSource.add("San Francisco");
+        citiesSource.add("London");
+        citiesSource.add("Paris");
+        citiesSource.add("Istanbul");
+        citiesSource.add("Berlin");
+        citiesSource.add("Barcelona");
+        citiesSource.add("Rome");
+         
+        cities = new DualListModel<>(citiesSource, citiesTarget);
+         
+    }
+ 
+    public DualListModel<String> getCities() {
+        return cities;
+    }
+ 
+    public void setCities(DualListModel<String> cities) {
+        this.cities = cities;
+    }
+ 
+ 
+    public void onSelect(SelectEvent event) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Item Selected", event.getObject().toString()));
+    }
+     
+    public void onUnselect(UnselectEvent event) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Item Unselected", event.getObject().toString()));
+    }
+     
+    public void onReorder() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "List Reordered", null));
+    } 
+}
---
+++ src/java/fr/epsi/mtp/i4/c22016/ThemeSwitcher/ThemeSwitcherView.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.epsi.mtp.i4.c22016.ThemeSwitcher;
+
+import fr.epsi.mtp.i4.c22016.models.Theme;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+ 
+@ManagedBean
+public class ThemeSwitcherView {
+ 
+    private List<Theme> themes;
+    private Theme theme;
+    
+   public void saveTheme() {
+      setTheme(getTheme());
+   } 
+    
+    @ManagedProperty("#{themeService}")
+    private ThemeService service;
+ 
+    @PostConstruct
+    public void init() {
+        themes = service.getThemes();
+        theme = new Theme(7, "Bootstrap", "bootstrap");
+    }
+     
+    public List<Theme> getThemes() {
+        return themes;
+    } 
+ 
+    public void setService(ThemeService service) {
+        this.service = service;
+    }
+
+    public Theme getTheme() {
+        return theme;
+    }
+
+    public void setTheme(Theme theme) {
+        this.theme = theme;
+    }
+}
